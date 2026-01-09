8885eebf7af8c9cc47e16198e0b49331f20253c7
Lucas Hansen <lucashansenwork_job@gmail.com>
2026-01-09 13:34:38 -0300

    Refactor code using functions

--- meu_primeiro_programa.js
+++ meu_primeiro_programa.js
@@ -1,24 +1,47 @@
 // Meu primeiro programa em JavaScript
 // Autor: Lucas Hansen
 
-const prompt = require('prompt-sync')();
+function verificarNumeroImparouPar(numero) {
+    if (numero % 2 === 0) {
+        console.log("O numero é par");
+    } else {
+        console.log("O numero é impar");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const prompt = require("prompt-sync")();
 //Solicitar ao usuário que insira um número
 const numero = parseInt(prompt("Digite um número inteiro: "));
 
 // Verificar se é par ou ímpar
-if (numero % 2 === 0) {
-    console.log("O número é par");
-} else {
-    console.log("O número é ímpar");
+function verificarNumeroImparouPar(numero) {
+    if (numero % 2 === 0) {
+        console.log("O número é par");
+    } else {
+        console.log("O número é ímpar");
+    }
 }
+// Verificar se é positivo, negativo ou zero
+function verificarsinaldoNumero(numero) {}
+    if(numero > 0) {
+        console.log("O numero é positivo");
+    } else if(numero < 0) {
+        console.log("O numero é negativo");
+    } else {
+        console.log("O numero é zero");
+    }
 
-if (numero > 0) {
-    console.log("O numero é positivo")
-} else if (numero < 0) 
-    console.log("O numero é negativo");
-else {
-    console.log("O numero é zero")
-}
 
 // Mostrar tabuada do numero
 console.log("Tabuada do " + numero + ":");
